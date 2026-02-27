dd9ff8cef246d5c8154e48e33d1c5796e54e645b
amanmedia053-oss <amanmedia053@gmail.com>
2026-02-26 22:04:27 -0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface Hadith {
+  TheNum: string;
+  Arabics: string;
+  Pashto: string;
+  Bab: string;
+  Bab_pashto: string;
+  Book: string;
+  Book_pashto: string;
+}
+
+export interface AppSettings {
+  arabicFontSize: number;
+  pashtoFontSize: number;
+  lineSpacing: number;
+  arabicFont: string;
+  animationsEnabled: boolean;
+  themeColor: string;
+}
+
+export interface BookGroup {
+  name: string;
+  pashtoName: string;
+  hadithCount: number;
+  chapters: ChapterGroup[];
+}
+
+export interface ChapterGroup {
+  name: string;
+  pashtoName: string;
+  hadiths: Hadith[];
+}
